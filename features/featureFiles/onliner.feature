097ef34c6fa633c7131abc6edd16911d5d9bbcdc
DESKTOP-7R8RD69\111 <a.ponomarenko@wellyes.ru>
2021-04-12 16:26:48 +0300

    done

--- features/featureFiles/onliner.feature
+++ features/featureFiles/onliner.feature
@@ -1,29 +1,16 @@
 @smoke
-Feature: Onliner showcase
+Feature: Onliner showcases
 
-        Scenario Outline: Clickcing "Барахолка" in onliner.by header
+              Examlple feature of UI automation
 
+        Scenario: Clickcing "Барахолка" in onliner.by header
             Given I am on Onliner homepage
              When I click "Барахолка" in Onliner.by header
              Then I redirected to  "Барахолка" page
 
-      
-       
-
+        Scenario: Search "Лопата" in onliner.by header
             Given I am on "Барахолка" page of onliner.by
              When I enter "Лопата" in Search field
-             Then I ckick "Search" button
-             Then I see "<ItemName>" item
-
-        Examples:
-                  | ItemName |
-                  | Все      |
-                  | Продам   |
-                  | Куплю    |
-                  | Обмен    |
-                  | Услуга   |
-                  | Аренда   |
-                  | Закрыто  |
-
-
+             Then I click "Search" button
+             Then I see following search result headers: "Все", "Продам", "Куплю", "Обмен", "Услуга", "Аренда", "Закрыто"
 
--- features/stepDefinitions/onliner.steps.ts
+++ features/stepDefinitions/onliner.steps.ts
@@ -1,23 +1,52 @@
 import { browser, by, element, ElementFinder, ExpectedConditions } from "protractor";
 
 
+
 export = function onlinerSteps() {
 
     //Default cucumber timeout
     this.setDefaultTimeout(60000);
 
     this.Given(/^I am on Onliner homepage$/, async () => {
-        let onlinerByURL:string = "https://www.onliner.by";
-        await browser.navigate().to(onlinerByURL);
+        let onlinerURL:string = "https://www.onliner.by/";
+        await browser.navigate().to(onlinerURL);
     });
 
     this.Then(/^I click "Барахолка" in Onliner.by header$/, async () => {
-        let barakholkaButton:ElementFinder = await element(by.xpath('//.b-main-navigation__text[contains(text(), "Барахолка")]'));
+        let barakholkaButton:ElementFinder = await element(by.xpath('//span[contains(text(), "Барахолка") and @class="b-main-navigation__text"]'));
         await barakholkaButton.click();
     });
 
     this.Then(/^I redirected to  "Барахолка" page$/, async () => {
         let barakholkaURL:string = "https://baraholka.onliner.by/";
         await ExpectedConditions.urlIs(barakholkaURL);
+    });
+
+    this.Given(/^I am on "Барахолка" page of onliner.by$/, async () => {
+        let onlinerURL:string = "https://www.onliner.by/";
+        let barakholkaButton:ElementFinder = await element(by.xpath('//span[contains(text(), "Барахолка") and @class="b-main-navigation__text"]'));
+        await browser.navigate().to(onlinerURL);
+        await barakholkaButton.click();
+    });
+
+    this.Then(/^I enter "Лопата" in Search field$/, async () => {
+        let searchField:ElementFinder = await element(by.xpath('//input[@id="fleaMarketSearchInput"]'));
+        await searchField.sendKeys("Лопата");
+    });
+
+    this.Then(/^I click "Search" button$/, async () => {
+        let searchButton:ElementFinder = await element(by.xpath('//button[@value="Найти"]'));
+        await searchButton.click();
+    });
+
+    this.Then(/^I see following search result headers: "Все", "Продам", "Куплю", "Обмен", "Услуга", "Аренда", "Закрыто"$/, async () => {
+        let elemArr:ElementFinder = await element(by.xpath('//ul[@class="b-ba-tabs fleamarket__1"]/child::*'));
+        let itemNamesArr:Array<string> = ["Все", "Продам", "Куплю", "Обмен", "Услуга", "Аренда", "Закрыто"];
+        
+        await function(){
+            for (let i = 0; i < elemArr.length; i++) {
+                ExpectedConditions.textToBePresentInElement(elemArr[i], itemNamesArr[i]);
+            };
+        };
     });
 };
